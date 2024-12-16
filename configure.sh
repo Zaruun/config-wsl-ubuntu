23de6b16c28476d0c9ccd9e0a7d89a50b68447d6
Matt <mucha.mateusz@hotmail.com>
2024-12-16 10:47:29 +0100

    feat: ansbile core tools and nvim

--- configure.sh
+++ configure.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo ""
-echo ""
-echo "configure.sh script"
-echo ""
-echo ""
+# Check if Ansible is installed
+if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    echo "Ansible is not installed. Installing..."
+
+    # Install Ansible
+    sudo apt update
+    sudo apt install -y ansible
+
+    echo "Ansible installed successfully."
+else
+    echo "Ansible is already installed."
+fi
+
+# Start ansible configuration playbook
+ansible-playbook -i localhost _ansible/playbook.yaml --ask-become-pass
