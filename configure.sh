ec2abb2ffa668c95654f6de7291ee6cc4a5b712f
Matt <mucha.mateusz@hotmail.com>
2024-12-16 11:31:23 +0100

    feat: clone dotfiles by ansible

--- configure.sh
+++ configure.sh
@@ -16,19 +16,3 @@ fi
 # Start ansible configuration playbook
 ansible-playbook -i localhost _ansible/playbook.yaml --ask-become-pass
 
-# Check if dotfiles dir exist if no clone it
-if [[ -d ~/dotfiles ]]; then
-    echo "[INFO] dotfiles directory already exist."
-else
-    repo_url="https://github.com/Zaruun/dotfiles"
-    dir="~/dotfiles"
-    echo "[INFO] Cloning repository from $repo_url to $dir..."
-    git clone "$repo_url" "$dir" || die "Failed to clone repository."
-
-    echo "[INFO] Changing directory to $dir..."
-    cd "$dir" || die "Failed to change directory to $dir."
-
-    echo "[INFO] Symlinking dotfiles..."
-    stow . || die "Error occurred while symlinking useing GNU stow."
-fi
- 
